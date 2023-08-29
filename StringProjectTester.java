96d2066ade44e65693f26c9e522137bd29e2a62c
wyattlake <lakewyatt@gmail.com>
2023-08-29 12:45:32 -0700

    Added countCharacters method and updated tester

--- StringProjectTester.java
+++ StringProjectTester.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
 
-public class StringProjectTester 
-{
-    public static void main(String[] args) throws IOException 
-	{
+public class StringProjectTester {
+	public static void main(String[] args) throws IOException {
 		StringProject tester = new StringProject();
-		tester.writeFile("this is what I want to write to the file");
-        System.out.println(tester.readFile("ReadThisFile"));
+		String input = "this is what I want to write to the file";
+		tester.writeFile(input);
+		System.out.println(tester.readFile("ReadThisFile"));
+		System.out.println("File length: " + tester.countCharacters("ReadThisFile"));
 	}
 }
